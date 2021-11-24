95fb796a51ff74efc2792542c67e0d2367a889ee
Ben Augood <ben_augood@outlook.com>
2021-11-24 16:08:25 +0000

    Commit master 4
    - Add reset button to clear todo list li elements

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // 2. On press reset, clear all li elements
 // add `<li>${newTask__input}</li>`
 
+const resetBtn = document.getElementById("hero_reset-btn")
+
 const taskList = document.getElementById("taskList__list");
 const taskText = document.getElementById("newTask__input");
 const taskInput = document.getElementById("newTask__input")
@@ -10,9 +12,16 @@ const taskNumber = document.getElementById("task__number")
 
 const createTask = () => {
     taskList.innerHTML += `<li>${taskInput.value}</li>`
+    taskInput.value = "";
 };
 
+const clearPage = () => {
+    taskList.innerHTML = ""
+}
+
 taskSubmit.addEventListener("click", createTask)
+
+resetBtn.addEventListener("click", clearPage)
 
 // if (taskList.innerHTML != "") {
 //     taskNumber.innerHTML = "Test"
